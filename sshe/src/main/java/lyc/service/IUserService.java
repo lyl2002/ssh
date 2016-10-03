39315787f4ac531e8154e4bf6ac324350cce1985
lyl2002 <3021217@qq.com>
2016-10-03 14:05:30 +0800

    1

---
+++ sshe/src/main/java/lyc/service/IUserService.java
@@ -0,0 +1,5 @@
+package lyc.service;
+
+public interface IUserService {
+	void myservice();
+}
---
+++ sshe/src/main/java/lyc/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package lyc.service.impl;
+
+import lyc.service.IUserService;
+
+public class UserServiceImpl implements IUserService {
+
+	@Override
+	public void myservice() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
