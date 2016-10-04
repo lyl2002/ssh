575e63c86fef7ea98d93adc612574f241cd4e277
lyl2002 <yunchuanluo@163.com>
2016-10-04 19:46:16 +0800

    1

--- sshe/src/main/java/lyc/service/impl/UserServiceImpl.java
+++ sshe/src/main/java/lyc/service/impl/UserServiceImpl.java
@@ -3,16 +3,20 @@ package lyc.service.impl;
 import lyc.service.IUserService;
 
 import org.apache.commons.logging.impl.LogKitLogger;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+
+
+
 @Service(value="userService")
 public class UserServiceImpl implements IUserService {
-
+	private static final Logger log = Logger.getLogger(UserServiceImpl.class);
 	@Override
 	public void myservice() {
 		// TODO Auto-generated method stub
 //		logger
-	
-		System.out.print("aaaaa");
+		log.error("aaaaaaaaaaaaaaaaaaaa");
+		//System.out.print("aaaaa");
 	}
 
 }
